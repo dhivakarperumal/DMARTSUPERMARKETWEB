39b161a01380d3e9710d0350254daf848fec851d
Dhivakar <142518349+dhivakarperumal@users.noreply.github.com>
2026-07-22 11:30:51 +0530

    fix it

--- Frontend/src/Admin/Staff/Staffs.jsx
+++ Frontend/src/Admin/Staff/Staffs.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import api from "../../api";
-import cache from "../../../cache";
+import cache from "../../cache";
 
 import {
   FaUsers,
